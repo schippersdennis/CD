4aa387f66509fcde539bb2c96b3a7630b974c5be
DS84 <schippersdennis@gmail.com>
2022-04-24 17:33:00 +0200

    adding functionality

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
-# Import what we need from flask
-from flask import Flask
+from flask import Flask, render_template, redirect, url_for
 
-# Create a Flask app inside `app`
 app = Flask(__name__)
 
-# Assign a function to be called when the path `/` is requested
+
 @app.route("/")
 def index():
-    return "CD Works 100%"
+    return render_template("index.html", title="Index")
 
 
-@app.route("/code")
+@app.route("/counter")
 def cow():
-    return "code route fixed 200%"
+    return render_template("counter.html", title="Counter")
 
