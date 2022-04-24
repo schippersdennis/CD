e4c798dfda51b676b27fbeab950e761c86d909da
DS84 <schippersdennis@gmail.com>
2022-04-24 09:57:00 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,15 @@
+# Import what we need from flask
+from flask import Flask
+
+# Create a Flask app inside `app`
+app = Flask(__name__)
+
+# Assign a function to be called when the path `/` is requested
+@app.route("/")
+def index():
+    return "Hello, CD!"
+
+
+@app.route("/code")
+def cow():
+    return "code"
